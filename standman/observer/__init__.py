2a3edd6927e4a94026b6ee2a87609774b5dde26a
minoru-jp <contactminoru@gmail.com>
2025-08-16 03:32:38 +0900

    Support installation via pip from local directory

--- standman/observer/__init__.py
+++ standman/observer/__init__.py
@@ -2,11 +2,8 @@
 from .observer import ProcessObserver
 from .observer import create_process_observer
 
-__version__ = '0.2.0'
-
 __all__ = (
     'ProcessObserver',
     'create_process_observer',
-    '__version__'
 )
 
